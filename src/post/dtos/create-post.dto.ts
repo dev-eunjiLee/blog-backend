55a0cfb0c48004ddc705ca141016c5c338110dce
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2025-08-03 01:25:03 +0900

    feat: createPost에 deletedUrlList 필드 추가

--- src/post/dtos/create-post.dto.ts
+++ src/post/dtos/create-post.dto.ts
@@ -1,9 +1,21 @@
-import { InputType, PickType } from '@nestjs/graphql';
+import { Field, InputType, IntersectionType, PickType } from '@nestjs/graphql';
 import { PostInputType } from '../post.entity';
+import { IsAlpha, IsArray, IsOptional, IsString } from 'class-validator';
+
+@InputType({ isAbstract: true })
+export class PostFileInputDto {
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  @Field(() => [String], {
+    nullable: true,
+    description: '삭제할 이미지 목록',
+  })
+  deletedUrlList?: string;
+}
 
 @InputType()
-export class CreatePostInputDto extends PickType(PostInputType, [
-  'title',
-  'content',
-  'hashtagList',
-]) {}
+export class CreatePostInputDto extends IntersectionType(
+  PickType(PostInputType, ['title', 'content', 'hashtagList']),
+  PostFileInputDto,
+) {}
